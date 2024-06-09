357ba09d4a66ac0e97f20630a1522b66965c9e2d
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-06-09 01:05:54 -0300

    refactor: single card page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.data import posts
-
+from typing import Any
+from django.http import HttpRequest, Http404
 # Create your views here.
 
 
@@ -20,19 +21,22 @@ def blog(request):
     )
 
 
-def post(request, post_id):
-    found_post = next((post for post in posts if post['id'] == post_id), None)
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = next(
+        (post for post in posts if post['id'] == post_id), None)
+
+    if found_post is None:
+        raise Http404('Post não existe.')
 
     context = {
-        'text': 'Estamos no Blog',
-        'title': 'Blogzao - ',
-        'posts': [found_post],
-        'post_id': post_id
+        'text': ' ',
+        'title': found_post['title'] + ' - ',
+        'post': found_post,
     }
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context
     )
 
